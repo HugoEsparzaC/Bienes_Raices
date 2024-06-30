4f06c5037a7c9a4eb63291c2ff4ae7c307a5f966
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2024-06-29 22:31:43 -0600

    actualizacion CRUD

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,6 +6,43 @@
     <main class="contenedor seccion">
         <h1>Crear</h1>
         <a href="/bienesraices/admin/index.php" class="boton boton-verde">Volver</a>
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 2" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="estacionamiento" id="habitaciones" placeholder="Ej: 1" min="1" max="9">
+            </fieldset>
+            <fieldset>
+                <legend>Vendedor</legend>
+                <select>
+                    <option value="1">Hugo</option>
+                    <option value="2">Carlos</option>
+                </select>
+            </fieldset>
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 <?php
