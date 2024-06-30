d6aabf09c54bc07030ef1ec92f2b7e9fdac5305f
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2024-06-29 20:26:10 -0600

    inicio CRUD

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -12,19 +12,19 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/bienesraices/index.php">
-                    <img src="build/img/logo.svg" alt="logotipo de bienes raices"/>
+                    <img src="/bienesraices/build/img/logo.svg" alt="logotipo de bienes raices"/>
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono-menu-responsive">
+                    <img src="/bienesraices/build/img/barras.svg" alt="icono-menu-responsive">
                 </div>
                 <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
+                    <img class="dark-mode-boton" src="/bienesraices/build/img/dark-mode.svg">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/bienesraices/nosotros.php">Nosotros</a>
+                        <a href="/bienesraices/anuncios.php">Anuncios</a>
+                        <a href="/bienesraices/blog.php">Blog</a>
+                        <a href="/bienesraices/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div>
